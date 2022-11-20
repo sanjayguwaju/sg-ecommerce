b14e0e0f8000ac7b2f0309860f481bfb4305b109
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-20 14:42:13 +0545

    slider page with image and description completed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
 import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+import Image1 from "../img/1-shop.png";
+import Image2 from "../img/2-shop.png";
+import Image3 from "../img/3-shop.png";
 
 const Container = styled.div`
   width: 100%;
@@ -26,6 +29,47 @@ const Arrow = styled.div`
   cursor: pointer;
   opacity: 0.5;
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
 const Slider = () => {
   return (
     <div>
@@ -33,6 +77,20 @@ const Slider = () => {
         <Arrow direction="left">
           <ArrowLeftOutlinedIcon />
         </Arrow>
+        <Wrapper>
+          <Slide>
+            <ImgContainer>
+              <Image src={Image1} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALE</Title>
+              <Desc>
+                DON'T COMPROMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS
+              </Desc>
+              <Button>SHOW WORK</Button>
+            </InfoContainer>
+          </Slide>
+        </Wrapper>
         <Arrow direction="right">
           <ArrowRightOutlinedIcon />
         </Arrow>
