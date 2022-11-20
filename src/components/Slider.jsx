a4d4219c53530bbee9545e870e087357986bcd08
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-20 13:02:43 +0545

    arrow of slider is added using direction and javascript in css

---
+++ src/components/Slider.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+const Slider = () => {
+  return (
+    <div>
+      <Container>
+        <Arrow direction="left">
+          <ArrowLeftOutlinedIcon />
+        </Arrow>
+        <Arrow direction="right">
+          <ArrowRightOutlinedIcon />
+        </Arrow>
+      </Container>
+    </div>
+  );
+};
+
+export default Slider;
