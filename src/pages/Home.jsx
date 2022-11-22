feedeb5d0bd1aee895ebb057931ed80f9249bac1
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-22 19:55:52 +0545

    Newsletter completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   );
 };
