19fee331622a1ccd9a187084fde8bf74ea81da0f
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-22 19:51:54 +0545

    Products page added where we can see products listed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   );
 };
