cefb222128b81de979157e5bb66bb18503b00612
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-22 19:51:30 +0545

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
-            <Badge badgeContent={4} color="primary">
+            <Badge badgeContent={4} color='primary'>
               <ShoppingCartOutlinedIcon />
             </Badge>
           </MenuItem>
