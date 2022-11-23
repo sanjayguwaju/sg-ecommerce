d92645b81b57efa3127051751de96670a7079103
sanjayguwaju <sanjayguwaju@gmail.com>
2022-11-23 19:31:22 +0545

    Register page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
 // import ProductList from "./pages/ProductList";
 
+import Register from "./pages/Register";
+
 const App = () => {
-  return <Product />;
+  return <Register />;
 };
 
 export default App;
