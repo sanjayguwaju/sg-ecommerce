f5502124f513e7eb4729a5f065caea1706268da1
sanjayguwaju <sanjayguwaju@gmail.com>
2022-12-02 23:53:27 +0545

    authentication and user login with jwt is done

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 //Importing configuration from dotenv file
 dotenv.config();
@@ -13,7 +14,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 }).catch((err) => {
     console.log(err);
 })
-
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 //Server Running in Port
