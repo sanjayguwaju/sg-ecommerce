3a003a174f694ce6c7c5e1c2cdda9224d96368ca
sanjayguwaju <sanjayguwaju@gmail.com>
2022-12-05 22:41:45 +0545

    delete user from its id

--- ecommerceapi/routes/user.js
+++ ecommerceapi/routes/user.js
@@ -1,10 +1,10 @@
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 
 
-//UPDATE
+//UPDATE methods has completed successfully where we can able to update the user data by verifyTokenAndAuthorization method
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
     if (req.body.password) {
@@ -23,6 +23,28 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
         res.status(200).json(updatedUser);
     }
     catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete for delete the user data by verifyTokenAndAuthorization method with id
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Users has been deleted ....");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+// GET USER by id and it can only make changes if  the user is admin
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
