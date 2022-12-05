daed37968e6effcd395724c683f8ddcaa7bbcb97
sanjayguwaju <sanjayguwaju@gmail.com>
2022-12-05 22:08:30 +0545

    update the user data

--- ecommerceapi/routes/user.js
+++ ecommerceapi/routes/user.js
@@ -1,7 +1,31 @@
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
+const User = require("../models/User");
 
-router.get("/usertest", () => {
-    console.log("usertest is successful");
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, {
+            $set: req.body,
+        },
+            { new: true }
+        );
+        res.status(200).json(updatedUser);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-module.exports = router
+module.exports = router;
+
