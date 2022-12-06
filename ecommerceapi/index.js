145262d977f81d46fff631dd2544d875791cc550
sanjayguwaju <sanjayguwaju@gmail.com>
2022-12-06 20:54:38 +0545

    cart Route and order Route added

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 //Importing configuration from dotenv file
 dotenv.config();
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 //Server Running in Port
 app.listen(process.env.PORT || 5000, () => {
