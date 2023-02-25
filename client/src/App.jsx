5c55873f3733351c7060f09e101398b8f969fc25
sanjayguwaju <sanjayguwaju@gmail.com>
2023-02-25 10:08:04 +0545

    fix: upgraded to react 18

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,14 @@
-// import Home from "./pages/Home";
 import React from "react";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
 
-// import Cart from "./pages/Cart";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Pay from "./components/Pay";
-import Success from "./components/Success";
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/pay' element={<Pay />}>
-        </Route>
-        <Route path='/success' element={<Success />}>
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return (<Home/>)
 };
 
 export default App;
