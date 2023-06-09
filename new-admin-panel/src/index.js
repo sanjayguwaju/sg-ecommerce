5db4cbdc13abcda4cfbf052fe83cf9024c7507d6
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-09 23:20:05 +0545

    removed: managed by removing files with new ones

--- new-admin-panel/src/index.js
+++ new-admin-panel/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { DarkModeContextProvider } from "./context/darkModeContext";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(
+  <DarkModeContextProvider>
+    <Provider store={store}>
+      <PersistGate loading="null" persistor={persistor}>
+        <App />
+    </PersistGate>
+    </Provider>
     </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
 );
