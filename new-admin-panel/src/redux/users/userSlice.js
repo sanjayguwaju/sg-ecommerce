fad06aadbf4a8284b8d1f5a1ad85d61b1da68369
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-17 18:56:37 +0545

    modified from what about coding learning

--- new-admin-panel/src/redux/users/userSlice.js
+++ new-admin-panel/src/redux/users/userSlice.js
@@ -1,29 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import {publicRequest,userRequest} from "../../requestMethods";
+import userService from "./userService";
 
-const userSlice = createSlice({
-  name: "user",
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await userRequest.post("/auth/register", userData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
   initialState: {
-    currentUser: null,
-    isFetching: false,
-    error: false,
+    user: [],
+    isLoading: false,
+    isSuccess: false,
+    error: null,
   },
-  reducers: {
-    loginStart: (state) => {
-      state.isFetching = true;
-    },
-    loginSuccess: (state, action) => {
-      state.isFetching = false;
-      state.currentUser = action.payload;
-    },
-    loginFailure: (state) => {
-      state.isFetching = false;
-      state.error = true;
-    },
-    logout: (state) => {
-      state.currentUser = null;
-    },
+  extraReducers: (builder) => {
+    builder
+      // Register User
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+        console.log(action.payload);
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+        console.log(action.payload);
+      })
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
-export default userSlice.reducer;
+export default authSlice.reducer;
