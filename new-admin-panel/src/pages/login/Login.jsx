96a7277d5d0ce4893e0a9c02f9402bc821c554ff
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-17 18:54:36 +0545

    added: login page and css

--- new-admin-panel/src/pages/login/Login.jsx
+++ new-admin-panel/src/pages/login/Login.jsx
@@ -1,9 +1,49 @@
-import "./login.scss"
+import React, { useState } from 'react';
+import './login.scss';
 
-const Login = () => {
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className="form-group">
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
 
-export default Login
+export default LoginForm;
