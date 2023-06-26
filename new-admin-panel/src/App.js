6c7c39c809e9afdd9af4ce2080d2d4107fca3e9a
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-26 14:23:14 +0545

    feat: login functionality with redirect to dashboard done

--- new-admin-panel/src/App.js
+++ new-admin-panel/src/App.js
@@ -7,30 +7,45 @@ import RegisterForm from "./pages/register/Register";
 import ForgetPasswordForm from "./pages/forgetpassword/ForgetPassword";
 import SignupForm from "./pages/signup/SignupForm";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
-import { useContext } from "react";
+import {useEffect,useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userLocalStorage = localStorage.getItem("persist:root");
+    const userJson = JSON.parse(userLocalStorage)?.user;
+    const isSuccess = JSON.parse(userJson)?.isSuccess;
+
+    console.log(isSuccess)
+
+    if (isSuccess) {
+      // Redirect to home page if isSuccess is true
+      navigate('/')
+    } else if (window.location.pathname !== "/register" || window.location.pathname !== "/forgetpassword") {
+      navigate('/login')
+    }
+  }, [navigate]);
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
-      <BrowserRouter>
         <Routes>
+          <Route exact path="login" element={<Login />} />
+          <Route exact path="register" element={<RegisterForm />} />
+          <Route exact path="forgetpassword" element={<ForgetPasswordForm />} />
+          <Route path="signup" element={<SignupForm />} />
           <Route path="/">
             <Route index element={<Home />} />
-            <Route exact path="login" element={<Login />} />
-            <Route exact path="register" element={<RegisterForm />} />
-            <Route exact path="forgetpassword" element={<ForgetPasswordForm />} />
-            <Route path="signup" element={<SignupForm />} />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
               <Route
-                path="new"
+                path="new" 
                 element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
@@ -44,7 +59,6 @@ function App() {
             </Route>
           </Route>
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
