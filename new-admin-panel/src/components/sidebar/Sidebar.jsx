ca076b0848b29a99ae25b74c6934e4f6384c48bd
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-26 14:56:52 +0545

    feat: logout functionality added

--- new-admin-panel/src/components/sidebar/Sidebar.jsx
+++ new-admin-panel/src/components/sidebar/Sidebar.jsx
@@ -11,12 +11,19 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import { Link } from "react-router-dom";
+import { Link, useNavigate  } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
 
 const Sidebar = () => {
   const { dispatch } = useContext(DarkModeContext);
+  const navigate = useNavigate();
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+    navigate("/login");
+  }
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -84,7 +91,7 @@ const Sidebar = () => {
           </li>
           <li>
             <ExitToAppIcon className="icon" />
-            <span>Logout</span>
+            <span onClick={clearLocalStorage}>Logout</span>
           </li>
         </ul>
       </div>
