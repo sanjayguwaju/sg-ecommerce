7c6eb5670efead9efcaf8e1998450e15a5c6fc33
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-11 16:12:04 +0545

    new list for the user list

---
+++ new-admin-panel/src/pages/product-list/List.jsx
@@ -0,0 +1,14 @@
+import "./list.scss"
+import Datatable from "../../components/datatable/Datatable"
+
+const List = () => {
+  return (
+    <div className="list">
+      <div className="listContainer">
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
+
+export default List
